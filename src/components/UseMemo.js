63fcb2761c3803773e0e2e935090948521a88e44
imrahulsrivastava <imrahulsrivastava1@gmail.com>
2024-10-08 22:36:28 +0530

    submission

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -40,11 +40,10 @@ const UseMemoComponent = () => {
 
       <section className="counter-section">
         <h2>Count: {counter}</h2>
-        <button
-          id="incr-btn incr-cnt"
-          onClick={handleIncrement}
-          className="button"
-        >
+        <button id="incr-btn" onClick={handleIncrement} className="button">
+          0
+        </button>
+        <button id="incr-cnt" onClick={handleIncrement} className="button">
           0
         </button>
       </section>
